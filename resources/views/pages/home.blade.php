f542c7cb54e00b389e5b9a105ea4dcf4363990f9
Kamal Lamichhane <lckamal@hotmail.com>
2015-09-21 20:55:15 +0545

    redirect to dashboard after login

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
             <h1>Asset Management</h1>
             <p>Weathered camera stimulate courier modem construct sentient artisanal vehicle order-flow plastic. Construct nodality range-rover drugs uplink systema jeans receding otaku shrine footage car film. Hacker sunglasses voodoo god cardboard Chiba katana systema savant-ware sprawl 3D-printed RAF fetishism kanji wristwatch. Free-market stimulate euro-pop corrupted post-paranoid franchise neon warehouse sunglasses tube katana shanty town man. </p>
             <p>
-                <a href="assets" class="btn btn-primary btn-lg">Enter</a>
+                <a href="home" class="btn btn-primary btn-lg">Enter</a>
             </p>
         </div>
     </div>
