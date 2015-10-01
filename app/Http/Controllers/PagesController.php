e526e51abccd690f09a3b112cb209a115b917a86
Kamal Lamichhane <lckamal@hotmail.com>
2015-10-01 22:13:00 +0545

    show dashboard if logged in from home

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
 {
@@ -21,6 +21,9 @@ class PagesController extends Controller
      */
     public function index()
     {
+        if (Auth::check()) {
+            return redirect('home');
+        }
         return View('pages.home');
     }
 
